8a7ebd4a029e7a25db344dd3d0eaa2d32e665d7b
zhanrui <zhanrui@gmail.com>
2013-05-20 17:43:48 +0800

    11

---
+++ src/resources/script/startup.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+    REM 设置批量CLASS路径
+    SET PRJ_HOME=D:\ccbprize
+
+    SET classpath=%JAVA_HOME%\jre\lib\alt-rt.jar;%JAVA_HOME%\jre\lib\charsets.jar;%JAVA_HOME%\jre\lib\rt.jar
+
+    SET classpath=%CLASSPATH%;%PRJ_HOME%\apps\ccbprize
+
+    FOR %%i IN (%PRJPATH%\lib\*.*) do call %PRJ_HOME%\bin\cpappend.bat %%i
+
+    SET CLASSPATH
+
+    JAVA prize.PosServer
